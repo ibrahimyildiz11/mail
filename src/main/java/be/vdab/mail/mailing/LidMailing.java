60d5a32a56f6c5d84209a55d7b04ebde66bddd55
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-05-08 06:21:14 +0000

    java mail @Async

--- src/main/java/be/vdab/mail/mailing/LidMailing.java
+++ src/main/java/be/vdab/mail/mailing/LidMailing.java
@@ -6,6 +6,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
@@ -17,6 +18,8 @@ public class LidMailing {
     public LidMailing(JavaMailSender sender) {
         this.sender = sender;
     }
+
+    @Async
     public void stuurMailNaRegistratie(Lid lid, String ledenURL) {
         try{
             var message = sender.createMimeMessage();
